dd890989f7c4e3f641a5da29b890bee13bdfe38c
RyanL0213 <tui02780@temple.edu>
2020-09-23 02:54:45 -0400

    adding features to onclick on button *if ones empty, error empty *if match, print name *if not match for either field, error incorrect

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -10,26 +10,39 @@ import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        String password = "ThisIsPassword";
-        String email = "myemail@email.com";
-        EditText useremail = findViewById(R.id.editTextTextEmailAddress);
-        EditText userpassword = findViewById(R.id.editTextTextPassword);
+        final String password = "ThisIsPassword";
+        final String email = "myemail@email.com";
+        final EditText useremail = findViewById(R.id.editTextTextEmailAddress);
+        final EditText userpassword = findViewById(R.id.editTextTextPassword);
 
         Button loginbutton = findViewById(R.id.button);
         loginbutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+            String userinputemail = useremail.getEditableText().toString();
+            String userinputpassword = userpassword.getEditableText().toString();
+            if(userinputemail.isEmpty()||userinputpassword.isEmpty())
+                Toast.makeText(FormActivity.this,"Please enter both of your email and password.",Toast.LENGTH_SHORT).show();
+            else if(userinputemail.equals(email)&&userinputpassword.equals(password))
+                Toast.makeText(FormActivity.this,"Welcome, Jane Dough, to the SignUpForm App",Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(FormActivity.this,"Login information is wrong, check your email and password.",Toast.LENGTH_SHORT).show();
 
-                Toast.makeText(FormActivity.this,"You have clicked the button",Toast.LENGTH_SHORT).show();
             }
+
 
 
         });
 
+//                Toast.makeText(MainActivity.this,"You have clicked the button",Toast.LENGTH_SHORT).show();
+
 }
+
 }
